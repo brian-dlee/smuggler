491e79d32d1aaa0c615e0ef6e6db4df7e1a9490c
Brian Lee <briandl92391@gmail.com>
2022-07-31 01:36:57 -0700

    slight demo cleanup

--- demo/app/routes/index.tsx
+++ demo/app/routes/index.tsx
@@ -1,33 +1,52 @@
-import type { LoaderFunction } from "@remix-run/node"
-import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import { load, debug } from "~/lib/config.server";
+import type { LoaderFunction } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import { load } from '~/lib/config.server';
 
 interface LoaderData {
   config: Record<string, string>;
   env: Record<string, string | undefined>;
 }
 
-export const loader: LoaderFunction = async () => {
-  try {
-    return json<LoaderData>({ config: await load(), env: process.env }, 200)
-  } catch (e) {
-    console.error("Failed to load configuation", e)
-    return json({ config: {}, env: {}, debug: await debug() }, 200)
-    // return json({ error: "configuration error" }, 500)
-  }
+function pick(
+  obj: Record<string, string | undefined>,
+  ...keys: string[]
+): Record<string, string | undefined> {
+  return Object.fromEntries(
+    Object.entries(obj).filter(([key]) => {
+      return keys.includes(key);
+    })
+  );
 }
 
+export const loader: LoaderFunction = async () => {
+  try {
+    return json<LoaderData>(
+      {
+        config: await load(),
+        env: pick(
+          process.env,
+          'SMUGGLER_ENCRYPTION_KEY',
+          'SMUGGLER_ENCRYPTION_IV',
+          'API_KEY',
+          'FILE_BASE64'
+        ),
+      },
+      200
+    );
+  } catch (e) {
+    return json({ error: 'configuration error' }, 500);
+  }
+};
+
 export default function Index() {
-  const { config, env, debug } = useLoaderData<LoaderData>()
+  const { config, env } = useLoaderData<LoaderData>();
 
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1>Smuggler Demo</h1>
       {config.FILE_BASE64 && <img src={`data:image/webp;base64,${config.FILE_BASE64}`} />}
-      <pre>
-        {JSON.stringify({ ...env, ...config }, null, 2)}
-      </pre>
+      <pre>{JSON.stringify({ ...env, ...config }, null, 2)}</pre>
     </div>
   );
 }
